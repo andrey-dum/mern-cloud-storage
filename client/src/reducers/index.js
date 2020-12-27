ca138792fdc9ce753b8ce467a30056b580853454
andrey-dum <duminskiy@gmail.com>
2020-12-27 15:43:44 +0200

    Redux, reducers, thunk

---
+++ client/src/reducers/index.js
@@ -0,0 +1,18 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import fileReducer from './fileReducer'
+import userReducer from './userReducer'
+
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    files: fileReducer
+})
+
+
+export const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(
+        thunk
+    )
+))
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    currentUser: {},
+    isAuth: false
+
+}
+
+export default function userReducer (state = initialState, action) {
+    switch (action.type) {
+        
+
+
+        default:
+            return state
+    }
+}
