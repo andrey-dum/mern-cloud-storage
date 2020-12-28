44ba505c20993f7aa8fc8a697a17392e1edcfab9
andrey-dum <duminskiy@gmail.com>
2020-12-28 21:18:35 +0200

    Drag and drop upload

--- client/src/components/Disk/Disk.jsx
+++ client/src/components/Disk/Disk.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { getFiles, uploadFile } from '../../actions/file'
 import FileList from './FileList'
@@ -11,6 +11,7 @@ export default function Disk() {
     const dispatch = useDispatch()
     const currentDir = useSelector(state => state.files.currentDir)
     const dirStack = useSelector(state => state.files.dirStack)
+    const [dragEnter, setDragEnter] = useState(false)
 
     useEffect(() => {
         dispatch(getFiles(currentDir))
@@ -30,8 +31,28 @@ export default function Disk() {
         files.forEach(file => dispatch(uploadFile(file, currentDir)))
     }
 
-    return (
-        <div className="disk">
+    function dragEnterHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        setDragEnter(true)
+    }
+
+    function dragLeaveHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        setDragEnter(false)
+    }
+
+    function dropHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        let files = [...event.dataTransfer.files]
+        files.forEach(file => dispatch(uploadFile(file, currentDir)))
+        setDragEnter(false)
+    }
+
+    return ( !dragEnter ? 
+        <div className="disk" onDragEnter={dragEnterHandler} onDragLeave={dragLeaveHandler} onDragOver={dragEnterHandler}>
             <Popup />
             <div className="disk__btns">
                 <button 
@@ -52,6 +73,9 @@ export default function Disk() {
                 </div>
             </div>
             <FileList />
+        </div>
+        : <div className="drop-area" onDrop={dropHandler} onDragEnter={dragEnterHandler} onDragLeave={dragLeaveHandler} onDragOver={dragEnterHandler}>
+            Перетащите файлы сюда
         </div>
     )
 }
